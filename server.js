94719ec52d0c64cb9edb61468be2f9ac0de80adf
rishabh <rishabhh2000@gmail.com>
2022-10-22 21:37:49 +0530

    Checkpoint 6 (Part 1)

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const db = require("./app/models");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+var corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mongoose Connection
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -13,20 +30,27 @@ db.mongoose
     process.exit();
   });
 
-const app = express();
-const port = 9000;
 
-app.get("/movies", (req, res) => {
-  res.send("All Movies Data in JSON format from Mongo DB");
+// Default route for the index or root path
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Upgrad Movie booking application development.",
+  });
 });
 
-app.get("/genres", (req, res) => {
-  res.send("All Genres Data in JSON format from Mongo DB");
-});
-
-app.get("/artists", (req, res) => {
-  res.send("All Artists Data in JSON format from Mongo DB");
-});
 
 app.listen(port, () => console.log("App listening on port: " + port));
 module.exports = app;
+
+// Hard-coded Routes are commented
+// app.get("/movies", (req, res) => {
+//   res.send("All Movies Data in JSON format from Mongo DB");
+// });
+
+// app.get("/genres", (req, res) => {
+//   res.send("All Genres Data in JSON format from Mongo DB");
+// });
+
+// app.get("/artists", (req, res) => {
+//   res.send("All Artists Data in JSON format from Mongo DB");
+// });
